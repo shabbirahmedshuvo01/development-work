0893b7febcee5a4c6a5a0594e45e930c025b032f
Shabbir Ahmed Shuvo <Shabbir Ahmed Shuvo>
2022-09-24 02:57:09 +0600

    new set

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -29,6 +29,31 @@ const Banner = () => {
                             <img src="https://i.ibb.co/1RNPK3X/dfshyrey.png" alt="" />
                         </div>
                     </div>
+                    <div key="2" className="flex justify-between gap-5 mx-auto bg-slate-300">
+                        <div className='flex-initial'>
+                            <h1 className='text-7xl'>We are different</h1>
+                            <h1 className=' mt-3 text-7xl'>From Other</h1>
+                            <p className=' my-10'>
+                                Our Online banking system
+                                project's aim is to automate transactions of the bank and provide better and faster service to the
+                                customers by using the internet.
+                            </p>
+
+
+                            <button className="btn btn-primary p-3 px-5 rounded-full">
+                                Get Started
+                            </button>
+
+
+                        </div>
+
+                        <div className=''>
+                            <img src="https://i.ibb.co/1RNPK3X/dfshyrey.png" alt="" />
+                        </div>
+                    </div>
+
+
+
                 </Slider>
             </section>
         </div>
