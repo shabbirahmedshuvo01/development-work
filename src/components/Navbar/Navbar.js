5a46547ae1d812be5afd3b00639707b9addd6c23
Shabbir Ahmed Shuvo <Shabbir Ahmed Shuvo>
2022-09-24 04:15:33 +0600

    updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -31,18 +31,18 @@ const Navbar = () => {
                     </div>
                     <div className="navbar-center hidden lg:flex">
                         <ul className="menu menu-horizontal p-0">
-                            <li><a>Item 1</a></li>
+                            <li><a>Products</a></li>
                             <li tabIndex="0">
                                 <a>
-                                    Parent
+                                    Pages
                                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                                 </a>
                                 <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
+                                    <li><a>Page 1</a></li>
+                                    <li><a>SubPage</a></li>
                                 </ul>
                             </li>
-                            <li><a>Item 3</a></li>
+                            <li><a>Blogs</a></li>
                         </ul>
                     </div>
                     <div className="navbar-end">
