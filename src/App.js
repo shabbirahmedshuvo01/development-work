0b6258783189c2405bf9c76309f7e294541c5f8a
Shabbir Ahmed Shuvo <Shabbir Ahmed Shuvo>
2022-09-24 02:52:02 +0600

    banner footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
+      <Footer />
     </div>
   );
 }
